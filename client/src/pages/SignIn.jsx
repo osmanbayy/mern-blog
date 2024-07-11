c7982c251f125606500129b12486af4466213ba4
osmanbayy <bayosman342mail.com>
2024-07-11 18:51:12 +0300

    complete signin page functionality

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,9 +1,125 @@
-
+import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 export default function SignIn() {
+  const [formData, setFormData] = useState({});
+  const [errorMessage, setErrorMessage] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id]: e.target.value.trim() });
+  };
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    if (!formData.email || !formData.password) {
+      return setErrorMessage("Please fill out all fields!");
+    }
+
+    try {
+      setLoading(true);
+      const res = await fetch("/api/auth/signin", {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+      const data = await res.json();
+
+      if (data.success === false) {
+        return setErrorMessage(data.message);
+      }
+      setLoading(false);
+
+      //kayıt başarılıysa home sayfasına yönlendir
+      if (res.ok) {
+        navigate("/");
+      }
+    } catch (err) {
+      setErrorMessage(err.message);
+      setLoading(false);
+    }
+  };
+
   return (
-    <div>
-      Sign In page
+    <div className="min-h-screen mt-20">
+      <div className="flex flex-col max-w-3xl gap-5 p-3 mx-auto md:flex-row md:items-center">
+        {/* left side */}
+        <div className="flex-1">
+          <Link to={"/"} className="text-4xl font-bold dark:text-white">
+            <span className="px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+              OBAY's
+            </span>
+            Blog
+          </Link>
+          <p className="mt-5 text-sm">
+            You can sign up with your email and password or with Google account.
+          </p>
+        </div>
+
+        {/* right side */}
+        <div className="flex-1">
+          <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+            {/* <div>
+              <Label value="Your username..." />
+              <TextInput
+                type="text"
+                placeholder="Username"
+                id="username"
+                autoComplete="off"
+                onChange={handleChange}
+              />
+            </div> */}
+            <div>
+              <Label value="Your email..." />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                autoComplete="off"
+                onChange={handleChange}
+              />
+            </div>
+            <div>
+              <Label value="Your password..." />
+              <TextInput
+                type="password"
+                placeholder="********"
+                id="password"
+                autoComplete="off"
+                onChange={handleChange}
+              />
+            </div>
+            <Button
+              gradientDuoTone="purpleToBlue"
+              type="submit"
+              disabled={loading}
+            >
+              {loading ? (
+                <>
+                  <Spinner size="sm" />
+                  <span className="pl-3">Loading...</span>
+                </>
+              ) : (
+                "Sign In"
+              )}
+            </Button>
+          </form>
+          <div className="flex gap-4 mt-5 text-sm">
+            <span>Don't have an account?</span>
+            <Link to={"/sign-up"} className="text-blue-600">
+              Sign Up
+            </Link>
+          </div>
+          {errorMessage && (
+            <Alert className="mt-5" color="failure">
+              {errorMessage}
+            </Alert>
+          )}
+        </div>
+      </div>
     </div>
-  )
+  );
 }
