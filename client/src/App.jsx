3ca2d312ac1317925771100ba314572694157273
osmanbayy <bayosman342mail.com>
2024-10-06 14:24:58 +0300

    create pages and routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
-
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
+import Dashboard from "./pages/Dashboard";
+import Projects from "./pages/Projects";
 
 export default function App() {
   return (
-    <div className="text-2xl text-red-500">
-      App
-    </div>
-  )
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/projects" element={<Projects />} />
+      </Routes>
+    </>
+  );
 }
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,8 @@
+
+export default function Home() {
+  return (
+    <div>
+      This is Home Page!
+    </div>
+  )
+}
