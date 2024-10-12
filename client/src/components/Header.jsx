e98c35c7999e870ba478c2bddcfe10cab0d1ec50
osmanbayy <bayosman342mail.com>
2024-10-12 23:17:00 +0300

    update header component with user data

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,33 +1,50 @@
-import {Button, Navbar, NavbarCollapse, NavbarLink, NavbarToggle, TextInput } from "flowbite-react";
+import {Avatar, Button, Dropdown, DropdownDivider, DropdownHeader, DropdownItem, Navbar, NavbarCollapse, NavbarLink, NavbarToggle, TextInput } from "flowbite-react";
 import { Link, useLocation } from "react-router-dom";
 import { IoSearchOutline } from "react-icons/io5";
 import { FaMoon } from "react-icons/fa";
+import { useSelector } from "react-redux";
 
 export default function Header() {
   const path = useLocation().pathname;
+  const { currentUser } = useSelector(state => state.user);
 
   return (
     <Navbar className="border-b-2">
-      <Link to={"/"} className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white">
-        <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+      <Link to={"/"} className="self-center text-sm font-semibold whitespace-nowrap sm:text-xl dark:text-white">
+        <span className="px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
           oBay&apos;s
         </span>
         Blog
       </Link>
 
-      <div className="flex gap-2 items-center md:order-2">
+      <div className="flex items-center gap-2 md:order-2">
         <form>
           <TextInput type="text" placeholder="Search..." rightIcon={IoSearchOutline} className="hidden lg:inline" />
         </form>
-        <Button className="w-12 h-10 flex items-center justify-center lg:hidden" color="gray" pill >
+        <Button className="flex items-center justify-center w-12 h-10 lg:hidden" color="gray" pill >
           <IoSearchOutline size={"18px"} />
         </Button>
-        <Button className="w-12 h-10 hidden sm:flex items-center " color="gray" pill >
+        <Button className="items-center hidden w-12 h-10 sm:flex " color="gray" pill >
           <FaMoon size={"14px"} />
         </Button>
-        <Link to={"/sign-in"}>
-          <Button gradientDuoTone="purpleToBlue" outline > Sign In </Button>
-        </Link>
+        {
+          currentUser ? 
+            <Dropdown arrowIcon={false} inline label={<Avatar alt="user" img={currentUser.profilePicture} rounded />}>
+              <DropdownHeader>
+                <span className="block text-sm">{currentUser.username}</span>
+                <span className="block text-sm font-medium truncate">{currentUser.email}</span>
+              </DropdownHeader>
+                <Link to={'/dashboard?tab=profile'}>
+                  <DropdownItem>Profile</DropdownItem>
+                </Link>
+                <DropdownDivider />
+                <DropdownItem>Sign Out</DropdownItem>
+            </Dropdown> : 
+            <Link to={"/sign-in"}>
+              <Button gradientDuoTone="purpleToBlue" outline > Sign In </Button>
+            </Link>
+        }
+        
         <NavbarToggle />
       </div>
 
