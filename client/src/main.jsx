508bcc0754aece3d087117c33bb6383f71fcd52e
osmanbayy <bayosman342mail.com>
2024-10-12 21:10:08 +0300

    add redux toolkit

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,14 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./redux/store.js";
+import { Provider } from "react-redux";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-)
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
+);
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,12 +1,14 @@
 import { Alert, Button, Label, Spinner, TextInput } from "flowbite-react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { signInStart, signInSuccess, signInFailure } from "../redux/user/userSlice.js";
+import { useDispatch, useSelector } from "react-redux";
 
 export default function SignIn() {
 
   const [formData, setFormData] = useState({});
-  const [errorMessage, setErrorMessage] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error: errorMessage } = useSelector(state => state.user);
+  const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleChange = (e) => {
@@ -16,11 +18,10 @@ export default function SignIn() {
   const handleSubmit = async (e) => {
     e.preventDefault();
     if(!formData.email || !formData.password){
-      return setErrorMessage('Please fill out all fields!');
+      return dispatch(signInFailure('Please fill out all fields!'));
     }
     try {
-      setLoading(true);
-      setErrorMessage(null);
+      dispatch(signInStart())
       const response = await fetch('/api/auth/signin', {
         method: 'POST',
         headers: { 'Content-type': 'application/json' },
@@ -29,34 +30,33 @@ export default function SignIn() {
       const data =  await response.json();
 
       if(data.success === false){
-        return setErrorMessage(data.message);
+        dispatch(signInFailure(data.message));
       }
       // if all functionality is true, stop loading
-      setLoading(false);
 
       if(response.ok){
+        dispatch(signInSuccess(data))
         navigate('/home');
       }
 
     } catch (error) {
-      setErrorMessage(error.message);
-      setLoading(false);
+      dispatch(signInFailure(error.message));
     }
   }
 
   return (
     <div className="min-h-screen mt-20">
-      <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
+      <div className="flex flex-col max-w-3xl gap-5 p-3 mx-auto md:flex-row md:items-center">
 
         {/* Left Side */}
         <div className="flex-1">
           <Link to={""} className="text-4xl font-bold dark:text-white">
-            <span className="px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white">
+            <span className="px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
               oBay&apos;s
             </span>
             Blog
           </Link>
-          <p className="text-sm mt-5">This is a demo project! You can sign in with your email and password or with Google.</p>
+          <p className="mt-5 text-sm">This is a demo project! You can sign in with your email and password or with Google.</p>
         </div>
 
         {/* Right Side */}
@@ -83,7 +83,7 @@ export default function SignIn() {
           </form>
 
           {/* Sign In Section */}
-          <div className="flex gap-2 text-sm mt-5">
+          <div className="flex gap-2 mt-5 text-sm">
             <span>Don&apos;t have an account?</span>
             <Link to={'/sign-in'} className="text-blue-500"> Sign Up</Link>
           </div>
