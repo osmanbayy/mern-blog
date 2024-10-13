c1b0a949a8387dcd3e64fea24245ccf564492ce1
osmanbayy <bayosman342mail.com>
2024-10-14 01:50:49 +0300

    update user  api route

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
 import authRoutes from "./routes/auth.route.js";
+import cookieParser from "cookie-parser";
 
 dotenv.config();
 
@@ -13,6 +14,8 @@ mongoose
 
 const app = express();
 app.use(express.json());
+
+app.use(cookieParser());
 
 app.listen(3000, () => console.log("Server is running on: 3000"));
 
