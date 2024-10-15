ba47813fa5869ef5a9223b5d732be0116e5046d9
osmanbayy <bayosman342mail.com>
2024-10-15 19:55:05 +0300

    add posts section to the dashboard

--- client/src/components/DashSidebar.jsx
+++ client/src/components/DashSidebar.jsx
@@ -4,9 +4,12 @@ import { FaSignOutAlt } from "react-icons/fa";
 import { FaUser } from "react-icons/fa6";
 import { Link, useLocation } from "react-router-dom";
 import { signoutSuccess } from "../redux/user/userSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { IoIosDocument } from "react-icons/io";
 
 export default function DashSidebar() {
+
+    const { currentUser } = useSelector((state) => state.user);
 
     const location = useLocation();
     const [tab, setTab] = useState("");
@@ -42,10 +45,17 @@ export default function DashSidebar() {
   return (
     <Sidebar className="w-full md:w-56">
       <SidebarItems>
-        <SidebarItemGroup>
+        <SidebarItemGroup className="flex flex-col gap-1">
             <Link to='/dashboard?tab=profile'>
-            <SidebarItem as="div" className="cursor-pointer" active={tab === 'profile'} icon={FaUser} label={'User'} labelColor='dark'>Profile</SidebarItem>
+              <SidebarItem as="div" className="cursor-pointer" active={tab === 'profile'} icon={FaUser} label={currentUser.isAdmin ? 'Admin' : 'User'} labelColor='dark'>Profile</SidebarItem>
             </Link>
+            {
+              currentUser.isAdmin && (
+                <Link to='/dashboard?tab=posts'>
+                  <SidebarItem as="div" className="cursor-pointer" active={tab === 'posts'} icon={IoIosDocument}>Posts</SidebarItem>
+                </Link>
+              )
+            }
             <SidebarItem onClick={handleSignout} className="cursor-pointer" icon={FaSignOutAlt} labelColor='dark'>Sign Out</SidebarItem>
         </SidebarItemGroup>
       </SidebarItems>
