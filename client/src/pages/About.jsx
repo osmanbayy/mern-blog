762db7cefb48adf830cf809b4ac6b4131bdd3967
osmanbayy <bayosman342mail.com>
2024-10-23 03:39:38 +0300

    complete about and projects pages

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,8 +1,33 @@
-
 export default function About() {
   return (
-    <div>
-      This is About Page
+    <div className="min-h-screen mx-auto">
+      <div className="max-w-2xl p-3 mx-auto mt-16">
+        <div className="">
+          <h1 className="flex items-center justify-center gap-3 pt-16 text-3xl font-semibold">
+            <p>About</p>
+            <p>
+              <span className="px-2 py-1 text-white rounded-lg bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500">
+                oBay&apos;s
+              </span>
+              Blog
+            </p>
+          </h1>
+          <div className="flex flex-col gap-6 pt-16 text-center text-gray-500 text-md">
+            <p>
+              Welcome to oBay&apos;s Blog! This blog was created by Osman Bay as
+              a personal project to share his thoughts and indeas with the
+              world. Osman is passionate developer who loves to write about
+              technology, coding and everything in between.
+            </p>
+            <p>
+              oBay&apos;s Blog is a blog that I created to share my thoughts and
+              ides with the world. I&apos;m a Full Stack Developer and I love to
+              write about my expreiences and things that I have learned. I hope
+              you enjoy reading my blog.
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
--- client/src/pages/Projects.jsx
+++ client/src/pages/Projects.jsx
@@ -1,8 +1,11 @@
+import CallToAction from "../components/CallToAction";
 
 export default function Projects() {
   return (
-    <div>
-      
+    <div className="flex flex-col items-center max-w-2xl min-h-screen gap-6 p-3 mx-auto mt-16">
+      <h1 className="text-3xl font-semibold">Projects</h1>
+      <p className="text-gray-500 text-md">Build fun and engaging projects while learnin HTML, CSS and JavaScript!</p>
+      <CallToAction />
     </div>
   )
 }
